a8cabb93de6a3abe39280497cd31758ab0c6c3c2
avgttt <117731256+avgttt@users.noreply.github.com>
2023-06-24 22:25:46 +0800

    update
    update

--- src/masternode-payments.h
+++ src/masternode-payments.h
@@ -30,8 +30,8 @@ static const int64_t FOUNDATION = 2000 * COIN;
 //  vote for masternode and be elected as a payment winner
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70222;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70222;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70223;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70223;
 
 extern CCriticalSection cs_vecPayees;
 extern CCriticalSection cs_mapMasternodeBlocks;
--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70222;
+static const int PROTOCOL_VERSION = 70223;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -19,7 +19,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70220;
+static const int MIN_PEER_PROTO_VERSION = 70223;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -42,6 +42,6 @@ static const int NO_BLOOM_VERSION = 70201;
 static const int SENDHEADERS_VERSION = 70201;
 
 //! DIP0001 was activated in this version
-static const int DIP0001_PROTOCOL_VERSION = 70222;
+static const int DIP0001_PROTOCOL_VERSION = 70223;
 
 #endif // BITCOIN_VERSION_H
