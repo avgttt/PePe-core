fe02eb27bc70febbaf9797d431cb6b88cde22f0c
MattF42 <matt_foster@me.com>
2024-02-02 18:26:16 +0000

    Introduce Protocol 70313

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70312;
+static const int PROTOCOL_VERSION = 70313;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,6 +22,7 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION = 70301;
 
 static const int MIN_PEER_SPORK_15 = 70312;
+static const int MIN_PEER_SPORK_15_POST_1070280 = 70313; // We stalled at 1070288
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
