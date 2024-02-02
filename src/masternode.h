9c879fe577dc41ee713a21ce0fe5f376ec928187
MattF42 <matt_foster@me.com>
2024-02-02 14:54:55 +0000

    Lengthen watchdog MN timeouts

--- src/masternode.h
+++ src/masternode.h
@@ -17,8 +17,8 @@ static const int MASTERNODE_CHECK_SECONDS               =   5;
 static const int MASTERNODE_MIN_MNB_SECONDS             =   5 * 60;
 static const int MASTERNODE_MIN_MNP_SECONDS             =  10 * 60;
 static const int MASTERNODE_EXPIRATION_SECONDS          =  65 * 60;
-static const int MASTERNODE_WATCHDOG_MAX_SECONDS        = 120 * 60;
-static const int MASTERNODE_NEW_START_REQUIRED_SECONDS  = 180 * 60;
+static const int MASTERNODE_WATCHDOG_MAX_SECONDS        = 150 * 60;
+static const int MASTERNODE_NEW_START_REQUIRED_SECONDS  = 210 * 60;
 
 static const int MASTERNODE_POSE_BAN_MAX_SCORE          = 2;
 
